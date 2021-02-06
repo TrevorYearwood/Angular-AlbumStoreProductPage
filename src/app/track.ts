5f680cba2dbfcbadb8e6717a2e1dcb420adab526
TrevorYearwood <trev_yearwood@hotmail.com>
2021-02-06 19:26:27 +0000

    Part 4

---
+++ src/app/track.ts
@@ -0,0 +1,8 @@
+import { NumberValueAccessor } from "@angular/forms/src/directives";
+
+export interface Track {
+    trackNumber: number;
+    trackName: string;
+    trackLength: string;
+    trackPrice: number;
+}
